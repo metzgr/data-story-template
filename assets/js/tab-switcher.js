b8443c5bd04a77173f7598dbbcfca026177ed460
(Noah) Hanlun Song <hsong@air.org>
2018-09-25 16:09:36 -0400

    add in all the tab content

--- assets/js/tab-switcher.js
+++ assets/js/tab-switcher.js
@@ -4,9 +4,9 @@
 (function () {
     "use strict";
 
-    window.TabSwitcher = function (tab_id, tab_content) {
+    window.TabSwitcher = function (tab_id, tab_content, content_class, tab_class) {
         // first of all we get all tab content blocks (I think the best way to get them by class names)
-        let all_content = document.getElementsByClassName("tabcontent");
+        let all_content = document.getElementsByClassName(content_class);
         let i;
         for (i = 0; i < all_content.length; i++) {
             all_content[i].style.display = 'none'; // hide all tab content
@@ -14,11 +14,11 @@
         document.getElementById(tab_content).style.display = 'block'; // display the content of the tab we need
 
         // now we get all tab menu items by class names (use the next code only if you need to highlight current tab)
-        let all_tab = document.getElementsByClassName("tabmenu");
+        let all_tab = document.getElementsByClassName(tab_class);
         let j;
         for (j = 0; j < all_tab.length; j++) {
-            all_tab[j].className = 'tabmenu';
+            all_tab[j].className = tab_class;
         }
-        document.getElementById(tab_id).className = 'tabmenu active';
+        document.getElementById(tab_id).className = tab_class + ' active';
     };
 })();
