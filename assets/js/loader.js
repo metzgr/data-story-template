60dd0abc8c22efdc89224decd65a5f13dba67532
metzgr <metzgr@users.noreply.github.com>
2018-09-30 11:26:33 -0400

    Delay animation until page load

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -22,3 +22,11 @@ $(document).ready(function() {
     $('body').addClass('loaded');
 
 });
+
+document.body.classList.add('js-loading');
+
+window.addEventListener("load", showPage, false);
+
+function showPage() {
+  document.body.classList.remove('js-loading');
+}
