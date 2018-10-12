44918211648e006ee69058d361b973c9fe2fb247
(Noah) Hanlun Song <hsong@air.org>
2018-10-12 14:22:29 -0400

    modify scroll.js so that is adds .not-ative-class to non-active list items

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -5,82 +5,79 @@
   // scroll progress bar
   // ---
 
-  const pixelsTag = document.querySelector("div.pixels")
-  const bodyTag = document.querySelector("body")
-  const progressTag = document.querySelector("div.progress")
-  const sections = document.querySelectorAll("section")
-  const clientTag = document.querySelector("div.client")
-  const pageTag = document.querySelector("div.page")
-  const headerTag = document.querySelector("header")
+  const pixelsTag = document.querySelector("div.pixels");
+  const bodyTag = document.querySelector("body");
+  const progressTag = document.querySelector("div.progress");
+  const sections = document.querySelectorAll("section");
+  const clientTag = document.querySelector("div.client");
+  const pageTag = document.querySelector("div.page");
+  const headerTag = document.querySelector("header");
 
   // when we scroll the page, update the pixels tag to be how far we've scrolled
   window.addEventListener("scroll", function () {
-    const pixels = window.pageYOffset
+    const pixels = window.pageYOffset;
 
     if (pixelsTag) {
-      pixelsTag.innerHTML = pixels
+      pixelsTag.innerHTML = pixels;
     }
-  })
+  });
 
   // when we scroll the page, make a progress bar that track of the distance
   window.addEventListener("scroll", function () {
-    const pixels = window.pageYOffset
-    const pageHeight = bodyTag.getBoundingClientRect().height
-    const totalScrollableDistance = pageHeight - window.innerHeight
+    const pixels = window.pageYOffset;
+    const pageHeight = bodyTag.getBoundingClientRect().height;
+    const totalScrollableDistance = pageHeight - window.innerHeight;
+    const percentage = pixels / totalScrollableDistance;
 
-    const percentage = pixels / totalScrollableDistance
-
-    progressTag.style.width = `${100 * percentage}%`
-  })
+    progressTag.style.width = `${100 * percentage}%`;
+  });
 
   // when we scroll the page, see how far down the page we've scrolled
   // then for each section, check whether we've passed it and if we have...
   // then updated the text in the header
   window.addEventListener("scroll", function () {
-    const pixels = window.pageYOffset
+    const pixels = window.pageYOffset;
 
     sections.forEach(section => {
       if (section.offsetTop - 60 <= pixels) {
         if (clientTag) {
-          clientTag.innerHTML = section.getAttribute("data-client")
+          clientTag.innerHTML = section.getAttribute("data-client");
         }
         if (pageTag) {
-          pageTag.innerHTML = section.getAttribute("data-page")
+          pageTag.innerHTML = section.getAttribute("data-page");
         }
         if (section.hasAttribute("data-is-dark")) {
-          headerTag.classList.add("white")
-          progressTag.classList.add("white")
+          headerTag.classList.add("white");
+          progressTag.classList.add("white");
         } else {
-          headerTag.classList.remove("white")
-          progressTag.classList.remove("white")
+          headerTag.classList.remove("white");
+          progressTag.classList.remove("white");
         }
       }
-    })
-  })
+    });
+  });
 
   // when user scrolls the page, make things parallax
   // we want to move certain tags, based on how far they are from an anchor point
   // what is the anchor? well its the middle of the section
   // how far should we parallax? well, it's a ratio of the middle distance scrolled to the middle point of the anchor
   window.addEventListener("scroll", function () {
-    const topViewport = window.pageYOffset
-    const midViewport = topViewport + (window.innerHeight / 2)
+    const topViewport = window.pageYOffset;
+    const midViewport = topViewport + (window.innerHeight / 2);
 
     sections.forEach(section => {
-      const topSection = section.offsetTop
-      const midSection = topSection + (section.offsetHeight / 2)
-
-      const distanceToSection = midViewport - midSection
-
-      const parallaxTags = section.querySelectorAll(`[data-parallax]`)
+      const topSection = section.offsetTop;
+      const midSection = topSection + (section.offsetHeight / 2);
+      const distanceToSection = midViewport - midSection;
+      const parallaxTags = section.querySelectorAll(`[data-parallax]`);
 
       // loop over each parallaxed tag
       parallaxTags.forEach(tag => {
-        const speed = parseFloat(tag.getAttribute("data-parallax"))
-        tag.style.transform = `translate(0, ${distanceToSection * speed}px)`
-      })
-    })
-  })
+        const speed = parseFloat(tag.getAttribute("data-parallax"));
+        tag.style.transform = `translate(0, ${distanceToSection * speed}px)`;
+      });
+    });
+  });
 
   // ---
   // shrink header when scroll
@@ -93,18 +90,20 @@
   // create the list of nav items with a ul
   let linkList = document.createElement("ul");
   let linkText = ["WHY ALGEBRA I", "WHO HAS ACCESS & WHERE", "WHO ENROLLS & WHERE"];
+
   linkText.forEach(function (text, index) {
     let listItem = document.createElement("li");
+    let listLink = document.createElement("a");
+    let textNode = document.createTextNode(text);
+
     if (index === 0) {
       listItem.setAttribute('class', 'active-nav');
     }
     else {
       listItem.setAttribute('class', 'not-active-nav');
     }
-    let listLink = document.createElement("a");
     listLink.style.lineHeight = "0px";
     listLink.setAttribute('href', "#" + text.replace(/\s/g, '').replace(/&/g, ""));
-    let textNode = document.createTextNode(text);
     listLink.appendChild(textNode);
     listItem.appendChild(listLink);
     linkList.appendChild(listItem);
@@ -150,9 +149,9 @@
   // scroll spy
   // ---
 
-  var sectionClasses = document.querySelectorAll(".section");
-  var sectionTopHeights = {};
-  var i = 0;
+  let sectionClasses = document.querySelectorAll(".section");
+  let sectionTopHeights = {};
+  let i = 0;
 
   Array.prototype.forEach.call(sectionClasses, function (e) {
     sectionTopHeights[e.id] = getPosition(e);
@@ -174,16 +173,12 @@
     if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
       for (i in sectionTopHeights) {
         if (sectionTopHeights[i] <= scrollPosition) {
-          document.querySelector('.active-nav').setAttribute('class', ' ');
+          document.querySelector('.active-nav').setAttribute('class', 'not-active-nav');
           document.querySelector("a[href='#" + i + "']").parentElement.setAttribute('class', 'active-nav');
         }
       }
     }
-    else {
-      document.querySelector('.not-active-nav').setAttribute('class', ' ');
-      document.querySelector("a[href='#" + i + "']").parentElement.setAttribute('class', 'not-active-nav');
-    }
-  };
+  }
 
   window.addEventListener('scroll', spyOnScroll);
 
