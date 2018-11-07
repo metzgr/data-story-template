10c92b10db65fe2f6667d0f484c279a708231df9
metzgr <metzgr@users.noreply.github.com>
2018-11-07 07:15:27 -0500

    Revised social sharing metadata

--- assets/js/social-share.js
+++ assets/js/social-share.js
@@ -2,5 +2,5 @@
 (function () {
     "use strict";
     document.querySelector("#share-facebook").href = "https://www.facebook.com/sharer/sharer.php?u=" + window.location.href;
-    document.querySelector("#share-twitter").href = "https://twitter.com/intent/tweet?status=STEM Data Story: +" + window.location.href;
+    document.querySelector("#share-twitter").href = "https://twitter.com/intent/tweet?status=A Leak in the STEM Pipeline: Taking Algebra Early. +" + window.location.href;
 })();
